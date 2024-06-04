b84db9eb40601032ec5318151e75dd2a51e840d2
Sameer Shaikh <sameer.shaikh.EXT@nmims.edu>
2024-06-04 16:16:36 +0530

    creation of centralized server and app level rooms

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ require("dotenv").config();
 console.log("ALLOWED_ORIGINS - ", JSON.parse(process.env.ALLOWED_ORIGINS));
 
 const io = SocketIo(httpServer, {
-    cors: {
-        origin: JSON.parse(process.env.ALLOWED_ORIGINS),
+  cors: {
+    origin: (origin, callback) => {
+      const allowedOrigins = JSON.parse(process.env.ALLOWED_ORIGINS);
+      const isOriginAllowed = allowedOrigins.some((allowedOrigin) => {
+        const regex = new RegExp(`^${allowedOrigin.replace("*", ".*")}$`);
+        return regex.test(origin);
+      });
+      if (isOriginAllowed || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
+  },
 });
 
 socketHandler(io);
 
 httpServer.listen(3009, () => {
-    console.log("Socket Server running on port 3009");
+  console.log("Socket Server running on port 3009");
 });
